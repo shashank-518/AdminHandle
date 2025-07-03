64fe5e2704eb5f4424d5e9bda637732cb8fdb7fa
Shashank S <shashank5418shashu@gmail.com>
2025-07-03 17:57:44 +0530

    Changes in middleware

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,6 +1,30 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware , clerkClient, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default clerkMiddleware()
+
+const isPublicRoute = createRouteMatcher(  [
+    "/",
+    "/api/webhook/register",
+    "/sign-up",
+    "/sign-in"
+])
+
+export default clerkMiddleware(async (auth , request)=>{
+    if (isPublicRoute(request)) {
+    return NextResponse.next(); // Allow access to public routes
+  }
+
+  // Protect private routes
+  const user = auth();
+
+  if (!user) {
+    // User not logged in, redirect to /sign-up
+    return NextResponse.redirect(new URL('/sign-up', request.url));
+  }
+
+  // If authenticated, allow access
+  return NextResponse.next();
+})
 
 export const config = {
   matcher: [
